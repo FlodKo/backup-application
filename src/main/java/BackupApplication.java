065df281dec3e868faf4b9c1f04d46d93dda5072
lwinterfeldt <luiswinterfeldt@riseup.net>
2023-04-11 14:57:35 +0200

    edited

--- src/main/java/BackupApplication.java
+++ src/main/java/BackupApplication.java
@@ -1,6 +1,6 @@
 public class BackupApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hallo, die Anwendung läuft jetzt");
+        System.out.println("Hallo, die Anwendung läuft jetzt noch besser");
     }
 }

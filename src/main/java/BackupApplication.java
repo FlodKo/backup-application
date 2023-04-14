5e0854dab0096ad80264eebbe66d5f5ac1291024
lwinterfeldt <luiswinterfeldt@riseup.net>
2023-04-14 17:00:38 +0200

    coping multiple files

--- src/main/java/BackupApplication.java
+++ src/main/java/BackupApplication.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 
 public class BackupApplication {
@@ -9,11 +10,28 @@ public class BackupApplication {
         this.sourceFile = sourceFile;
         this.targetFile = targetFile;
     }
+    public static void backup (File sourceDirectory, File targetDirectory) {
+        if (sourceDirectory.listFiles().length == 0) {
+            return;
+        }
+        for (File file : sourceDirectory.listFiles()) {
+            File newTargetDirectory = new File(targetDirectory.toPath().resolve(Path.of(file.getName(),"")).toUri());
+            if (!file.isDirectory()) {
+                copySingleFile(file, newTargetDirectory);
+            } else {
+                if (!targetDirectory.isDirectory()){
+                    targetDirectory.delete();
+                }
+                if (!targetDirectory.exists()) {
+                    targetDirectory.mkdir();
+                }
+                newTargetDirectory.mkdir();
+                backup(file, newTargetDirectory);
+            }
+        }
 
-
-
-
-    public static void copySingleFile(File file, File destinationDirectory)  {
+    }
+    private static void copySingleFile(File file, File destinationDirectory)  {
         try {
             if (!destinationDirectory.exists()) {
                 destinationDirectory.createNewFile();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo, die Anwendung läuft jetzt noch viel besser");
         //BackupApplication backupApplication = new BackupApplication(new File("./src/main/java/Main.java"), new File("./"));
-        BackupApplication.copySingleFile(new File("./src/main/java/Main.java"), new File("./backup"));
-
+        BackupApplication.backup(new File("./src"), new File("./backup"));
     }
 
 
